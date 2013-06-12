8da6408cb6371bb64b9654dd91aa589203d45d30
Jason Leng <cool_curly@hotmail.com>
2013-06-11 21:40:20 -0700

    Seeded one experience

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Experience.delete_all
+
+# Seed an experience
+Experience.create tagline: "Check out a mural in the mission district"
