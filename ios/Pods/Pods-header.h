b0c3fd492c266c27ec485b73ad615e962b105a92
George Dean <gdean123@gmail.com>
2013-06-17 13:14:47 -0700

    Add OCMock pod

--- ios/Pods/Pods-header.h
+++ ios/Pods/Pods-header.h
@@ -2,3 +2,11 @@
 #define __COCOA_PODS
 
 #define __POD_AFNetworking
+#define __POD_RestKit
+#define __POD_RestKit_Core
+#define __POD_RestKit_CoreData
+#define __POD_RestKit_Network
+#define __POD_RestKit_ObjectMapping
+#define __POD_RestKit_Support
+#define __POD_SOCKit
+#define __POD_TransitionKit
