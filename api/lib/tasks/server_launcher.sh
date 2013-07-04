585f5ae2fc693807cfd17dc2d927cd3f394d5264
George Dean <gdean123@gmail.com>
2013-07-04 11:22:18 -0700

    Create new integration environment in api for ios integration tests

--- api/lib/tasks/server_launcher.sh
+++ api/lib/tasks/server_launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pwd=`pwd`
 osascript -e "tell application \"Terminal\" to do script \"cd $pwd; rails s\""
-osascript -e "tell application \"Terminal\" to do script \"cd $pwd; RAILS_ENV=test rails s -p 3001 -P tmp/pids/server2.pid\""
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; RAILS_ENV=integration rails s -p 3001 -P tmp/pids/server2.pid\""
