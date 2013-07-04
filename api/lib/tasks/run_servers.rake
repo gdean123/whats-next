9266ae71810b1b3757ebeec4d20c385d10bf03ea
George Dean <gdean123@gmail.com>
2013-07-04 12:30:53 -0700

    Use foreman instead of custom server_launcher

--- api/lib/tasks/run_servers.rake
+++
@@ -1,9 +0,0 @@
-namespace :servers do
-  desc "Run two rails servers, one for dev and another for test"
-
-  task :run_servers => :environment do
-    pwd = Dir.pwd
-    exec("cd #{pwd}; cd lib/tasks; ./server_launcher.sh")
-  end
-
-end
--- api/lib/tasks/server_launcher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pwd=`pwd`
-osascript -e "tell application \"Terminal\" to do script \"cd $pwd; rails s\""
-osascript -e "tell application \"Terminal\" to do script \"cd $pwd; RAILS_ENV=integration rails s -p 3001 -P tmp/pids/server2.pid\""
