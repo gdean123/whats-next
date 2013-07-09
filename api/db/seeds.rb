1e6a88115c0e945a816c3d70c05b7b0297a5f7a2
George Dean <gdean123@gmail.com>
2013-07-08 20:19:40 -0700

    Reorder and revise seed experiences

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,60 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 Experience.destroy_all
-
-# Seed an experience
-Experience.create tagline: "Check out a mural in the mission district",
-                  image: "mural.png",
-                  latitude: 37.768476,
-                  longitude: -122.415388
-
-
-Experience.create tagline: "Visit the parrots on telegraph hill",
-                  image: "parrots.png",
-                  latitude: 37.802223,
-                  longitude: -122.405659
 
 Experience.create tagline: "Visit the birds near shoreline lake",
                   image: "birds.png",
                   latitude: 37.431774,
                   longitude: -122.086706
 
-Experience.create tagline: "Little dolls in East West Bookshop",
-                  image: "book_store.jpg",
-                  latitude: 37.392493,
-                  longitude: -122.080039
-
-Experience.create tagline: "Nice sun set off bridge 84",
-                  image: "bridge.jpg",
-                  latitude: 37.512449,
-                  longitude: -122.118927
-
-Experience.create tagline: "Hang out and have fun in the Castro street, Mountain View",
-                  image: "castro_st.jpg",
-                  latitude: 37.394164,
-                  longitude: -122.079055
-
-Experience.create tagline: "The biggest 'like' sign",
-                  image: "facebook.png",
-                  latitude: 37.481738,
-                  longitude: -122.150076
-
 Experience.create tagline: "Check out the fish in Seascapes Fish & Pets",
                   image: "fish.jpg",
                   latitude: 37.394982,
                   longitude: -122.07992
 
-Experience.create tagline: "Come to see the flowers",
+Experience.create tagline: "Look at the blossoms in Shoreline park",
                   image: "flowers.png",
                   latitude: 37.400915,
                   longitude: -122.094887
+
+Experience.create tagline: "Play with the little dolls in East West Bookshop",
+                  image: "book_store.jpg",
+                  latitude: 37.392493,
+                  longitude: -122.080039
+
+Experience.create tagline: "Watch the sunset on the Dumbarton bridge",
+                  image: "bridge.jpg",
+                  latitude: 37.512449,
+                  longitude: -122.118927
+
+Experience.create tagline: "Hang out and have fun on Castro Street",
+                  image: "castro_st.jpg",
+                  latitude: 37.394164,
+                  longitude: -122.079055
+
+Experience.create tagline: "Revel in the biggest 'like' sign ever",
+                  image: "facebook.png",
+                  latitude: 37.481738,
+                  longitude: -122.150076
 
 Experience.create tagline: "Visit the Rengstorff House",
                   image: "rengstoff_house.png",
                   latitude: 37.432102,
                   longitude: -122.087166
+
+Experience.create tagline: "Check out a mural in the mission district",
+                  image: "mural.png",
+                  latitude: 37.768476,
+                  longitude: -122.415388
+
+Experience.create tagline: "Visit the parrots on telegraph hill",
+                  image: "parrots.png",
+                  latitude: 37.802223,
+                  longitude: -122.405659
