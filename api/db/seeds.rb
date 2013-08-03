b78069d7bf6a61746d86f5871b555a050f77f262
George Dean <gdean123@gmail.com>
2013-08-03 15:25:01 -0700

    Update seeds to reference files directly on S3

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,51 +1,51 @@
 Experience.destroy_all
 
 Experience.create tagline: "Visit the birds near shoreline lake",
-                  image: "birds.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/birds.png",
                   latitude: 37.431774,
                   longitude: -122.086706
 
 Experience.create tagline: "Check out the fish in Seascapes Fish & Pets",
-                  image: "fish.jpg",
+                  image_url: "https://s3.amazonaws.com/whatsnext/fish.jpg",
                   latitude: 37.394982,
                   longitude: -122.07992
 
 Experience.create tagline: "Look at the blossoms in Shoreline park",
-                  image: "flowers.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/flowers.png",
                   latitude: 37.400915,
                   longitude: -122.094887
 
 Experience.create tagline: "Watch the sunset on the Dumbarton bridge",
-                  image: "bridge.jpg",
+                  image_url: "https://s3.amazonaws.com/whatsnext/bridge.jpg",
                   latitude: 37.512449,
                   longitude: -122.118927
 
 Experience.create tagline: "Hang out and have fun on Castro Street",
-                  image: "castro_st.jpg",
+                  image_url: "https://s3.amazonaws.com/whatsnext/castro_st.jpg",
                   latitude: 37.394164,
                   longitude: -122.079055
 
 Experience.create tagline: "Revel in the biggest 'like' sign ever",
-                  image: "facebook.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/facebook.png",
                   latitude: 37.481738,
                   longitude: -122.150076
 
 Experience.create tagline: "Visit the Rengstorff House",
-                  image: "rengstoff_house.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/rengstoff_house.png",
                   latitude: 37.432102,
                   longitude: -122.087166
 
 Experience.create tagline: "Check out a mural in the mission district",
-                  image: "mural.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/mural.png",
                   latitude: 37.768476,
                   longitude: -122.415388
 
 Experience.create tagline: "Visit the parrots on telegraph hill",
-                  image: "parrots.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/parrots.png",
                   latitude: 37.802223,
                   longitude: -122.405659
 
 Experience.create tagline: "Have coffee and read a book at Borderlands Cafe",
-                  image: "book.png",
+                  image_url: "https://s3.amazonaws.com/whatsnext/book.png",
                   latitude: 37.759248,
                   longitude: -122.421452
